82e3b171169b9ba4b687c10003df9c0b31c0d79a
Timo Johannsen <timo.johannsen@zf.com>
2025-04-15 15:09:39 +0200

    initial commit

---
+++ src/main/java/de/dhbwravensburg/webeng2/vorlesungdemo/VorlesungDemoApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.webeng2.vorlesungdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VorlesungDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VorlesungDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dhbwravensburg/webeng2/vorlesungdemo/VorlesungDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.webeng2.vorlesungdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorlesungDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
